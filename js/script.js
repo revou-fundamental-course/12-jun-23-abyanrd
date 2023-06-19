60028f8bac6d85f232ef6e4332fe780b407365bc
Abyan <abyanpn86@gmail.com>
2023-06-20 00:21:11 +0700

    konversi suhu berhasil

--- js/script.js
+++ js/script.js
@@ -1,10 +1,30 @@
-let cKeF = (("") * 9/5) + 32 ("°F");
-let fKeC = (("") - 32) * 5/9 ("°C");
+//ini rumus
+// let cKeF = (("celsius") * 9/5) + 32 ("°F");
+// let fKeC = (("fahrenheit") - 32) * 5/9 ("°C");
 
-function konversi(){
-    let input = document.getElementById("celsiusField").value;
-    let hasil = (parseInt(input) * 9/5) + 32;
-    console.log(hasil + "°F");
-    var output = document.getElementById("fahrenheitField");
-    output.innerHTML = hasil;
+function konversi() {
+    // ambil nilai input dari kolom Celsius
+    const celsiusInput = document.getElementById('inputField');
+    const celsiusValue = parseFloat(celsiusInput.value);
+
+    // konversi ke Fahrenheit
+    const fahrenheitValue = (celsiusValue * 9/5) + 32;
+
+    // menampilkan hasil konversi di kolom output
+    const outputField = document.getElementById('outputField');
+    outputField.value = fahrenheitValue + ('°F')
+
+    // menampilkan metode perhitungan di kolom calculation method
+    const methodField = document.getElementById('methodField');
+    methodField.value = celsiusValue + (' * 9/5 + 32 = ') + outputField.value;
+}
+
+function reset() {
+    document.getElementById('inputField').value = '';
+    document.getElementById('outputField').value = '';
+    document.getElementById('methodField').value = '';
+}
+
+function reverse() {
+
 }
